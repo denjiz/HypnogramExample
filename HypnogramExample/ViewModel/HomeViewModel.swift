1ad1e12ae05bbd99485958886856ee987c954eb4
Denis Mendica <mendica.denis@gmail.com>
2024-11-09 11:23:06 +0100

    Create a basic chart

---
+++ HypnogramExample/ViewModel/HomeViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewModel.swift
+//  HypnogramExample
+//
+//  Created by Denis Mendica on 09.11.2024..
+//
+
+import Foundation
+
+struct HypnogramMarkData: Identifiable {
+    let id = UUID()
+    let date: Date
+    let phase: String
+}
+
+class HomeViewModel: ObservableObject {
+    
+}
