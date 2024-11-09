e515407222239ae6e6d7422481834aa26efe89fe
Denis Mendica <mendica.denis@gmail.com>
2024-11-09 09:05:29 +0100

    Initial commit

---
+++ HypnogramExample/HypnogramExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HypnogramExampleApp.swift
+//  HypnogramExample
+//
+//  Created by Denis Mendica on 09.11.2024..
+//
+
+import SwiftUI
+
+@main
+struct HypnogramExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
